277af39ca7d555de0b80de64d830bc41c3c38f70
Viktor Benei <viktor.benei@gmail.com>
2014-10-22 00:23:38 +0200

    environment: 'IsExpand' option added - if it's false then the environment value won't be expanded before it gets added to the environment

--- _scripts/make_osx.sh
+++ _scripts/make_osx.sh
@@ -6,7 +6,7 @@ go build
 
 # move the created 'main' binary into 'bin/step_agent_osx'
 echo "Moving to bin..."
-mkdir -p bin
-mv step-agent-go bin/step_agent_osx
+mkdir -p _bin
+mv step-agent-go _bin/step_agent_osx
 
 echo "Done"
